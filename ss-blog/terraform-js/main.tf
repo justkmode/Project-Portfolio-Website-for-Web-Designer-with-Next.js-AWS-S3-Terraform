7e2ec0785430c3be98df6bcf6ea4dbdea8ba5e51
justkmode <136920389+justkmode@users.noreply.github.com>
2024-08-03 22:03:26 +0200

    Update main.tf

--- ss-blog/terraform-js/main.tf
+++ ss-blog/terraform-js/main.tf
@@ -2,55 +2,92 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "nextjs_bucket" {
-  bucket = "nextjs-portfolio-bucket-ss"
+# Import existing S3 Bucket
+resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
+  bucket = "nextjs-portfolio-bucket-ss-justyna"
+  acl    = "private"
 }
 
-# Ownership Control
 resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
-  bucket = aws_s3_bucket.nextjs_bucket.id
+  bucket = "nextjs-portfolio-bucket-ss-justyna"
 
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-# Block Public Access
 resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block" {
-  bucket = aws_s3_bucket.nextjs_bucket.id
-
   block_public_acls       = false
   block_public_policy     = false
+  bucket                  = "nextjs-portfolio-bucket-ss-justyna"
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-# Bucket ACL
-resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.nextjs_bucket_ownership_controls,
-    aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
-  ]
 
-  bucket = aws_s3_bucket.nextjs_bucket.id
-  acl    = "public-read"
-}
-
-# Bucket Policy
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
-  bucket = aws_s3_bucket.nextjs_bucket.id
+  bucket = aws_s3_bucket_acl.nextjs_bucket_acl.bucket
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.nextjs_bucket.arn}/*"
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket_acl.nextjs_bucket_acl.bucket}/*"
       }
     ]
   })
+}
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket_acl.nextjs_bucket_acl.bucket}.s3.amazonaws.com"
+    origin_id   = "s3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
